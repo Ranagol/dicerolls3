49f3ae026586993cf2ff1aafc46032cea698219f
Andor <andorhorvat@gmail.com>
2019-04-16 13:49:06 +0200

    Optimalisation done.

--- dice.php
+++ dice.php
@@ -17,7 +17,7 @@ class Dice{
 
 	public function __construct($randomNumberMinimum, $maxDiceNumber){
 		$this->maxDiceNumber = $maxDiceNumber;
-		$this->randomNumber = 10;//rand($randomNumberMinimum,$maxDiceNumber);//here we create a random number between the min and max dice value
+		$this->randomNumber = rand($randomNumberMinimum,$maxDiceNumber);//here we create a random number between the min and max dice value
 			
 				switch ($this->randomNumber){
 				case '1':
@@ -55,10 +55,93 @@ class Dice{
 	}
 
 
+	public function dot1($imSquare, $blueDot){		
+		$dot5 = imagefilledellipse($imSquare, 30, 30, 9, 9, $blueDot);
+		return $imSquare;		
+	}
+
+	public function dot2($imSquare, $blueDot){
+		$dot1 = imagefilledellipse($imSquare, 12, 12, 9, 9, $blueDot);
+		$dot9 = imagefilledellipse($imSquare, 48, 48, 9, 9, $blueDot);
+		return $imSquare;
+	}
+
+	public function dot3($imSquare, $blueDot){
+		$dot1 = imagefilledellipse($imSquare, 12, 12, 9, 9, $blueDot);
+		$dot5 = imagefilledellipse($imSquare, 30, 30, 9, 9, $blueDot);
+		$dot9 = imagefilledellipse($imSquare, 48, 48, 9, 9, $blueDot);
+		return $imSquare;
+	}
+
+	public function dot4($imSquare, $blueDot){
+		$dot1 = imagefilledellipse($imSquare, 12, 12, 9, 9, $blueDot);		
+		$dot3 = imagefilledellipse($imSquare, 48, 12, 9, 9, $blueDot);		
+		$dot7 = imagefilledellipse($imSquare, 12, 48, 9, 9, $blueDot);
+		$dot9 = imagefilledellipse($imSquare, 48, 48, 9, 9, $blueDot);
+		return $imSquare;
+	}
+
+	public function dot5($imSquare, $blueDot){
+		$dot1 = imagefilledellipse($imSquare, 12, 12, 9, 9, $blueDot);		
+		$dot3 = imagefilledellipse($imSquare, 48, 12, 9, 9, $blueDot);		
+		$dot5 = imagefilledellipse($imSquare, 30, 30, 9, 9, $blueDot);	
+		$dot7 = imagefilledellipse($imSquare, 12, 48, 9, 9, $blueDot);
+		$dot9 = imagefilledellipse($imSquare, 48, 48, 9, 9, $blueDot);
+		return $imSquare;
+	}
+
+	public function dot6($imSquare, $blueDot){
+		$dot1 = imagefilledellipse($imSquare, 12, 12, 9, 9, $blueDot);		
+		$dot3 = imagefilledellipse($imSquare, 48, 12, 9, 9, $blueDot);
+		$dot4 = imagefilledellipse($imSquare, 12, 30, 9, 9, $blueDot);	
+		$dot6 = imagefilledellipse($imSquare, 48, 30, 9, 9, $blueDot);
+		$dot7 = imagefilledellipse($imSquare, 12, 48, 9, 9, $blueDot);
+		$dot9 = imagefilledellipse($imSquare, 48, 48, 9, 9, $blueDot);
+		return $imSquare;
+	}
+
+	public function dot7($imSquare, $blueDot){
+		$dot1 = imagefilledellipse($imSquare, 12, 12, 9, 9, $blueDot);
+		$dot3 = imagefilledellipse($imSquare, 48, 12, 9, 9, $blueDot);
+		$dot4 = imagefilledellipse($imSquare, 12, 30, 9, 9, $blueDot);
+		$dot5 = imagefilledellipse($imSquare, 30, 30, 9, 9, $blueDot);
+		$dot6 = imagefilledellipse($imSquare, 48, 30, 9, 9, $blueDot);
+		$dot7 = imagefilledellipse($imSquare, 12, 48, 9, 9, $blueDot);
+		$dot9 = imagefilledellipse($imSquare, 48, 48, 9, 9, $blueDot);
+		return $imSquare;
+	}
+
+	public function dot8($imSquare, $blueDot){
+		$dot1 = imagefilledellipse($imSquare, 12, 12, 9, 9, $blueDot);
+		$dot2 = imagefilledellipse($imSquare, 30, 12, 9, 9, $blueDot);
+		$dot3 = imagefilledellipse($imSquare, 48, 12, 9, 9, $blueDot);
+		$dot4 = imagefilledellipse($imSquare, 12, 30, 9, 9, $blueDot);
+		$dot6 = imagefilledellipse($imSquare, 48, 30, 9, 9, $blueDot);
+		$dot7 = imagefilledellipse($imSquare, 12, 48, 9, 9, $blueDot);
+		$dot8 = imagefilledellipse($imSquare, 30, 48, 9, 9, $blueDot);
+		$dot9 = imagefilledellipse($imSquare, 48, 48, 9, 9, $blueDot);
+		return $imSquare;
+	}
+
+	public function dot9($imSquare, $blueDot){
+		$dot1 = imagefilledellipse($imSquare, 12, 12, 9, 9, $blueDot);
+		$dot2 = imagefilledellipse($imSquare, 30, 12, 9, 9, $blueDot);
+		$dot3 = imagefilledellipse($imSquare, 48, 12, 9, 9, $blueDot);
+		$dot4 = imagefilledellipse($imSquare, 12, 30, 9, 9, $blueDot);
+		$dot5 = imagefilledellipse($imSquare, 30, 30, 9, 9, $blueDot);
+		$dot6 = imagefilledellipse($imSquare, 48, 30, 9, 9, $blueDot);
+		$dot7 = imagefilledellipse($imSquare, 12, 48, 9, 9, $blueDot);
+		$dot8 = imagefilledellipse($imSquare, 30, 48, 9, 9, $blueDot);
+		$dot9 = imagefilledellipse($imSquare, 48, 48, 9, 9, $blueDot);
+		return $imSquare;
+	}
+
+
 //------------------------------------------
 	public function justDoIt(){//THIS IS THE FINAL COMMAND FOR CREATING A RECTANGLE, THAT INCLUDES ALL OTHER COMMANDS
 		$inProgress1 = $this->createRectangle();
-		$inProgress2 = $this->dots3($inProgress1['imSquare'], $inProgress1['blueDot']);
+		$functionName = 'dot' . $this->randomNumber;
+		$inProgress2 = $this->$functionName($inProgress1['imSquare'], $inProgress1['blueDot']);
 		$this->saveImage($inProgress2);
 	}
 
@@ -68,16 +151,10 @@ class Dice{
 		$blueDot = imagecolorallocate($imSquare, 10, 10, 255);//blue
 		$yellow = imagecolorallocate($imSquare, 255, 255, 0);
 		ImageFilledRectangle($imSquare,2,2,56,56,$yellow);
-
 		return ['imSquare' => $imSquare, 'blueDot' => $blueDot];
 	}
 
-	public function dots3($imSquare, $blueDot){//THIS CREATES THE DOTS
-		$dot3 = imagefilledellipse($imSquare, 48, 12, 9, 9, $blueDot);
-		$dot5 = imagefilledellipse($imSquare, 30, 30, 9, 9, $blueDot);		
-		$dot7 = imagefilledellipse($imSquare, 12, 48, 9, 9, $blueDot);
-		return $imSquare;
-	}
+	
 
 	public function saveImage($imSquare){//THIS SAVES EVERYTHING INTO A .PNG FILE
 		$fileName = 'images/image' . $this->randomNumber . '.png';
@@ -92,37 +169,15 @@ class Dice{
 //---------------------------------
 
 
-
-
-
-
 	public function drawDiceNumeric(){//THIS IS THE PATTERN FOR ALL THE NUMBERS ABOVE 9
-		//creating the rectangle, defining the colors
-		
-		//unlink($this->string);
-		/*
-		$imSquareX =  imageCreate(60, 60);//this is the whole image
-		$background_color = imagecolorallocate($imSquareX, 255, 255, 255);
-		$blueDot = imagecolorallocate($imSquareX, 10, 10, 255);//blue
-		$yellow = imagecolorallocate($imSquareX, 255, 255, 0);
-		ImageFilledRectangle($imSquareX,2,2,56,56,$yellow);
-		*/
-		//text part (number display)
-		
-		
 		
 		$imSquare = $this->createRectangle();
-
 		imagestring($imSquare['imSquare'],5,20,20,$this->randomNumber,$imSquare['blueDot']);
-
 		$array = [ 'images/', $this->randomNumber,'.', 'png'];//here we create a string from randomNumber a . and a png. This will be the name of the image.
 		$string = implode("",$array);
-
-		
 		imagepng($imSquare['imSquare'], $string);
 		imagedestroy($imSquare['imSquare']);
 		echo '<img src=' . $string . '>';
-		
 	}
 
 
